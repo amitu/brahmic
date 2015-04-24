318f85186a8004d7b2ccd81cba24bc7baa02c7ac
Amit Upadhyay <amitu@browserstack.com>
2015-04-24 22:39:57 +0530

    telugu readme

--- readmer.py
+++ readmer.py
@@ -1,10 +1,16 @@
-import cmu
+import cmu, telugu
+
 
 def main():
     readme = file("README.md").read().decode("utf-8")
     eng = readme.split("----")[0]
     hin = cmu.trans_text(eng)
-    file("README.md", "w").write(("%s----\n\n%s" % (eng, hin)).encode("utf-8"))
+    tel = telugu.trans_text(eng)
+    file("README.md", "w").write(
+        (
+            "%s----\n\n%s----\n\n%s" % (eng, hin, tel)
+        ).encode("utf-8")
+    )
 
 if __name__ == "__main__":
     main()
